31fdacde21ff952676b5fdc9fe531296deb6d7e4
nashafee <naghman.shafique@gmail.com>
2022-12-22 23:50:01 +0500

    Save TS Proj1

---
+++ Calculator/index.ts
@@ -0,0 +1,58 @@
+import inquirer from "inquirer";
+
+const answers: {
+    numberOne: number,
+    numberTwo: number,
+    operator: String
+} = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly Enter first Number:"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly Enter Second Number:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/", "%", "**"],
+        message: "Select you operator for Calcuation:"
+    },
+]);
+
+const { numberOne, numberTwo, operator } = answers;
+
+if (numberOne && numberTwo && operator) {
+    let result: number = 0;
+    
+    switch (operator) {
+        case "+":
+            result = numberOne + numberTwo;
+            break;
+        case "-":
+            result = numberOne - numberTwo;
+            break;
+        case "*":
+            result = numberOne * numberTwo;
+            break;
+        case "/":
+            result = numberOne / numberTwo;
+            break;
+        case "%":
+            result = numberOne % numberTwo;
+            break;
+        case "**":
+            result = numberOne ** numberTwo;
+            break;
+    }
+
+    console.log(`Your result is :${result}`);
+
+} else {
+    console.log("Kindly input your Numbers and Operator");
+
+}
+
